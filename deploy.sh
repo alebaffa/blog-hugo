519b4335f8d7d1ed06320cd4b67fd884c0f93150
Alessandro Baffa <alessandro.baffa@gmail.com>
2016-11-05 20:32:47 +0100

    up and running again

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-echo -e "\033[0;32mDeploying updates on alebaffa/hugo-blog ...\033[0m"
-git add -A
-msg="updating blog `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-git push origin master
 
-echo -e "\033[0;32mDeploying updated /public on to alebaffa.github.io ...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
