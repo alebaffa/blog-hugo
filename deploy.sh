ed26b052bd993a79c856f68cc3ac878524fadcef
Alessandro Baffa <alessandro.baffa@gmail.com>
2017-11-12 14:25:43 +0900

    rebuild site Sun, Nov 12, 2017  2:25:39 PM

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,28 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
+echo -e "move to /public"
 cd public
 # Add changes to git.
+echo -e "adding files..."
 git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="rebuild site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
-# Push source and build repos.
+remote=$(git remote -v)
+echo -e "pushing /public into $remote"
+
 git push origin master
 
 # Come Back
 cd ..
+git add -A
+git commit -m "$msg"
+remote=$(git remote -v)
+echo -e "pushing root into ${remote}"
