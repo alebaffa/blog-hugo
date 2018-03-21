dde851ea8f7754745fc783907fc2b6d5b3536c31
Alessandro Baffa <alessandro.baffa@gmail.com>
2018-03-21 21:07:57 +0900

    remove old deploy

--- old_deploy/init.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo -e "clone into /public"
-git clone https://github.com/alebaffa/alebaffa.github.io.git public
-echo -e "move into /public"
-cd public
-echo -e "set url to: git@github.com:alebaffa/alebaffa.github.io.git"
-git remote set-url origin git@github.com:alebaffa/alebaffa.github.io.git
